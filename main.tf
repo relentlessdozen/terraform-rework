0c50ce7210a6f047c4f40cb23c68f2c0bbc418fe
Rick James <patrickjamesjr@protonmail.com>
2020-02-06 21:22:23 -0500

    Added a test folder structure!

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "east-us-2"
 }
+
+resource "vpc" "dev-vpc-hbcc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name  = "dev-vpc-hbcc"
+    Group = "DevSecOps"
+  }
+}
