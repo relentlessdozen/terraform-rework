978759a85c24bdca9b797354740a1c5ea53fae97
Rick James <patrickjamesjr@protonmail.com>
2020-02-06 21:56:22 -0500

    Updated main with more resources. Should be in modules but not yet!

--- main.tf
+++ main.tf
@@ -10,3 +10,30 @@ resource "aws_vpc" "dev-vpc-hbcc" {
     Group = "DevSecOps"
   }
 }
+
+resource "aws_instance" "dev-ec2-www" {
+  ami                    = "ami-062f7200baf2fa504"
+  isntance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.dev-sg-hbcc.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Go to the sky!" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+  tags = {
+    Name  = "dev-ec2-www"
+    Group = "DevSecOps"
+  }
+}
+
+resource "aws_security_group" "dev-sg-hbcc" {
+  name = "www-sg-hbcc"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
